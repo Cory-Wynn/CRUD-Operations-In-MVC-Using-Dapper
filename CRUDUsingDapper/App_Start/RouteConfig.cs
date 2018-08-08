5bd5332daece47c56bd8637f927c961df15fa819
Cory Wynn <corywynn1993@gmail.com>
2018-08-08 15:52:07 +0100

    Added Update,Delete and add functionality and updated route config

--- CRUDUsingDapper/App_Start/RouteConfig.cs
+++ CRUDUsingDapper/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace CRUDUsingDapper
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Employee", action = "AddEmployee", id = UrlParameter.Optional }
             );
         }
     }
